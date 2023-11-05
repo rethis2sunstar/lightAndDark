7da2186b08685bb2aa9efe78769fa34f3188f9a5
rethissunstar <rethissunstar@gmail.com>
2023-11-05 12:43:46 -0500

    updated readme. removed test for button press. added transition

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lightAndDark
+
 This is the code to allow for a like and dark mode selector
+
+## Contributing code
+
+https://www.w3schools.com/howto/howto_css_switch.asp
--- assets/index.js
+++ assets/index.js
@@ -3,8 +3,6 @@ var bodyVar = document.body;
 var mode = "light";
 
 toggleBtn.addEventListener("click", function () {
-  console.log("Button clicked"); // Add this line to check if the event handler is working
-
   if (mode == "light") {
     mode = "dark";
     bodyVar.setAttribute("class", "dark");
